aade7ee6c183912c85cc9ed0e1879fd05eb953b2
brobint <105503817+brobint@users.noreply.github.com>
2022-05-13 09:22:13 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,20 @@
+$(".openbox-button").click(function() {
+    $(".secondImage").show();
+});
+
+$(".throwawayboxes-button").click(function() {
+    $(".secondImage").hide();
+});
+
+$(".aww").dblclick(function() {
+    $(".thirdimage").show();
+});
+
+
+$(".boxes").dblclick(function() {
+    $(".forthimage").show();
+});
+
+$(".now").dblclick(function() {
+    $(".fifthimage").show();
+});
